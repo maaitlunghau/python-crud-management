2eec5eec8bed3ac5253ca68602ccf3555a729725
MTH <chunhau.py@gmail.com>
2025-12-24 10:17:08 +0700

    feat(06_student_management): defined Person & Student class

---
+++ solutions/student_management/Person.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    # getter (property) & setter (value)
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def age(self):
+        return self._age
+    
+    @name.setter
+    def name(self, value):
+        self._name = value
+        
+    @age.setter
+    def age(self, value):
+        self._age = value
